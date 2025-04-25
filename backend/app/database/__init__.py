05ad4aa62f2f7c906a4366f2a8cdd20bd1ff4032
Shiny0706 <superdev9676@gmail.com>
2025-04-24 23:59:38 -0400

    Del: Deleted all data again caz gitignore not defined

--- backend/app/database/__init__.py
+++
@@ -1,8 +0,0 @@
-from app.database.db import Base, engine
-
-# Import all models here so they are registered with SQLAlchemy
-from app.models.survey import Survey
-
-# Create tables in the database
-def create_tables():
-    Base.metadata.create_all(bind=engine) 
--- backend/run.py
+++
@@ -1,9 +0,0 @@
-import uvicorn
-from app.database import create_tables
-
-if __name__ == "__main__":
-    # Create tables before starting the application
-    create_tables()
-    
-    # Start the FastAPI application
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True) 
